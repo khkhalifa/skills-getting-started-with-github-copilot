6fff912f10556230dd7628b7a1eaee5084c59a8e
Khaled Khalifa <33331600+khkhalifa@users.noreply.github.com>
2025-11-09 08:40:37 +0000

    Add participant list display to activities

--- src/static/app.js
+++ src/static/app.js
@@ -20,11 +20,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const spotsLeft = details.max_participants - details.participants.length;
 
+        // Create participants list HTML
+        let participantsHTML = '';
+        if (details.participants.length > 0) {
+          participantsHTML = `
+            <p><strong>Participants:</strong></p>
+            <ul class="participants-list">
+              ${details.participants.map(email => `<li>${email}</li>`).join('')}
+            </ul>
+          `;
+        } else {
+          participantsHTML = '<p><strong>Participants:</strong> <em>Be the first to sign up!</em></p>';
+        }
+
         activityCard.innerHTML = `
           <h4>${name}</h4>
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
+          ${participantsHTML}
         `;
 
         activitiesList.appendChild(activityCard);
